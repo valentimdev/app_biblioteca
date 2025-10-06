5a289792bbc64ec5f5e4fe4350fc805776a2431b
valentimdev <valentimdesenvolvedor@gmail.com>
2025-10-06 12:45:26 -0300

    novo botão de voltar e tela de cadastro quase pronta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding= true;
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
